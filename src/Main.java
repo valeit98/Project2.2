a5bbaaef391bed42cbec836d55fc048fee937762
Valeriy Dobrov <valeriy.dobrov.98@yandex.ru>
2024-02-01 22:38:58 +0400

    Add CamelCase

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int account = 100; //начальная сумма на счету
-        int accountbonus = 1100; //сумма пополнения
+        int accountBonus = 1100; //сумма пополнения
 
         int bonus;
 
-        if (accountbonus > 1000) {
-            bonus = accountbonus / 100; //расчёт бонусных средств за каждые 100 рублей
+        if (accountBonus > 1000) {
+            bonus = accountBonus / 100; //расчёт бонусных средств за каждые 100 рублей
         } else {
             bonus = 0;
         }
 
-        int balance = account + accountbonus + bonus;
+        int balance = account + accountBonus + bonus;
 
         System.out.println("Итоговая сумма на счету: " + balance + " руб.");
         System.out.println("Бонус составил: " + bonus + " руб.");
